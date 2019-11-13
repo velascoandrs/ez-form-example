e118b7b2948effbfd5e4de4cd562ec9a683041ed
velascoandrs <velasco.andrs@gmail.com>
2019-11-13 16:27:41 -0500

    upload example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,164 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ez-form-example';
+  myConfiguration = [
+    {
+      controlName: 'uuid',
+      type: {
+        typeName: 'input'
+      },
+      disabled: true,
+    },
+    {
+      controlName: 'password',
+      type: {
+        typeName: 'input',
+        class: 'password',
+      },
+      validators: [
+        Validators.required,
+      ]
+    },
+    {
+      controlName: 'birthday',
+      placeholder: 'Enter your birthday date',
+      type: {
+        typeName: 'date'
+      },
+      validators: [
+        Validators.required,
+      ]
+    },
+    {
+      controlName: 'otherDate',
+      placeholder: 'Enter a date',
+      type: {
+        typeName: 'date'
+      },
+      validators: [
+        Validators.required,
+      ],
+      errorMessages: {
+        required: 'The date is required',
+        date: 'The date is not acceptable'
+      },
+    },
+    {
+      controlName: 'email',
+      validators: [
+        Validators.required,
+        Validators.email
+      ],
+      placeholder: 'Enter an email',
+      type: {
+        typeName: 'input'
+      },
+      errorMessages: {
+        required: 'The email is mandatory',
+        email: 'You must enter a valid email',
+      },
+    },
+    {
+      controlName: 'civilState',
+      placeholder: 'Choose a civil state',
+      label: 'Civil state',
+      validators: [
+        Validators.required
+      ],
+      type: {
+        typeName: 'select',
+        options: [
+          {
+            value: 1,
+            label: 'Married'
+          },
+          {
+            value: 2,
+            label: 'Single'
+          }
+        ]
+      },
+    },
+    {
+      controlName: 'cities',
+      validators: [
+        Validators.required
+      ],
+      type: {
+        typeName: 'check',
+        options: [
+          {
+            value: 1,
+            label: 'Quito'
+          },
+          {
+            value: 2,
+            label: 'Cuenca'
+          },
+          {
+            value: 3,
+            label: 'Ambato'
+          }
+        ]
+      },
+      label: 'Cities',
+      errorMessages: {
+        required: 'select a city at least',
+      }
+    },
+    {
+      controlName: 'favoriteFruit',
+      validators: [
+        Validators.required
+      ],
+      label: 'Favorite Fruit',
+      type: {
+        typeName: 'radio',
+        options: [
+          {
+            value: 3,
+            label: 'Apple'
+          },
+          {
+            value: 1,
+            label: 'Pear'
+          },
+          {
+            value: 2,
+            label: 'Pineapple'
+          }
+        ],
+      },
+    }
+  ];
+
+  userData = {
+    uuid: 1234,
+    email: 'juan.pecados@mail.com',
+    civilState: 1,
+    otherDate: '2015-02-16',
+    birthday: '1999-02-16',
+    favoriteFruit: 1,
+    cities: [1, 3],
+    password: '12133',
+  };
+  myToasterConfig = {
+    success: {
+      type: 'info',
+      title: 'GOOD',
+      body: 'All right!!'
+    },
+    fail: {
+      type: 'warning',
+      title: 'BAD',
+      body: 'Someting was wrong!!'
+    }
+  };
+
+  someFunction(event) {
+    // Your logic ..
+    console.log(event);
+  }
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {EzFormModule} from '@gordon_freeman/ez-form';
+import {MatNativeDateModule} from '@angular/material';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    EzFormModule,
+    MatNativeDateModule,
   ],
-  providers: [],
+  providers: [
+    MatNativeDateModule,
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
